4c1491498ea94acc9741509d41baad85b73b6d0a
Malijk Persco <martinbosquez2015@gmail.com>
2026-01-21 14:15:41 +0100

    21-01-2026

---
+++ DAM1/Phyton/PycharmProjects1/PythonProject/tareas/TM_Tarea01.py
@@ -0,0 +1,27 @@
+paise_mapoblao={
+    "India": 1451,
+    "China": 1419,
+    "Estados Unidos": 345,
+    "Indonesia": 283,
+    "Pakistán": 251,
+    "Nigeria": 233,
+    "Brasil": 212,
+    "Bangladés": 173,
+    "Rusia": 145,
+    "Etiopía": 132,
+    "México": 131,
+    "Japón": 124,
+    "Egipto": 116,
+    "Filipinas": 116,
+    "República Democrática del Congo": 109
+}
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+print(paise_mapoblao[])
